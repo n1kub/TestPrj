f7ca0e509fbc3d94eaf6686394d09d75353aac5b
n1kub <n1kub@mail.ru>
2022-03-04 16:42:20 +0000

    first attempt to finish. There'll be one more.

--- html/index.php
+++ html/index.php
@@ -4,21 +4,25 @@ $loader = new Twig_Loader_Filesystem('../html');
 $twig = new Twig_Environment($loader); 
 require 'lib.php';
 
-$stud_tasks_answer = proc_call('stud_tasks', 1);
-$amount_lecturer_answer = proc_call('amount_of_studs_for_task_of_lecturer', 2);
-$task_sg_answer = proc_call('task_studs_groups', 3);
-$class_studs_answer = proc_call('class_studs_list', '\'Экономика в информационных технологиях\'');
-$lecturer_load_answer = proc_call('lecturer_amount_of_tasks');
-$task_load_answer = proc_call('task_load');
+$stud_tasks_answer = proc_call($student_tasks,1);
+$amount_lecturer_answer = proc_call($lec_grs, NULL, 2);
+
+
+// $stud_tasks_answer = proc_call('stud_tasks', 1);
+// $amount_lecturer_answer = proc_call('amount_of_studs_for_task_of_lecturer', 2);
+// $task_sg_answer = proc_call('task_studs_groups', 3);
+// $class_studs_answer = proc_call('class_studs_list', '\'Экономика в информационных технологиях\'');
+// $lecturer_load_answer = proc_call('lecturer_amount_of_tasks');
+// $task_load_answer = proc_call('task_load');
 
 
 echo $twig->render('template.html', array(
-    'stud_tasks' => $stud_tasks_answer, 'stud_tasks_keys' => array_keys($stud_tasks_answer[0]),
-    'amount_lecturer' => $amount_lecturer_answer, 'amount_lecturer_keys' => array_keys($amount_lecturer_answer[0]),
-    'task_sg' => $task_sg_answer, 'task_sg_keys' => array_keys($task_sg_answer[0]),
-    'class_studs' => $class_studs_answer, 'class_studs_keys' => array_keys($class_studs_answer[0]),
-    'lecturer_load' => $lecturer_load_answer, 
-    'task_load' => $task_load_answer)
+    'stud_tasks' => $stud_tasks_answer, 'stud_tasks_keys' => array_keys($stud_tasks_answer[0]),)
+//     'amount_lecturer' => $amount_lecturer_answer, 'amount_lecturer_keys' => array_keys($amount_lecturer_answer[0]),
+//     'task_sg' => $task_sg_answer, 'task_sg_keys' => array_keys($task_sg_answer[0]),
+//     'class_studs' => $class_studs_answer, 'class_studs_keys' => array_keys($class_studs_answer[0]),
+//     'lecturer_load' => $lecturer_load_answer, 
+//     'task_load' => $task_load_answer)
 ); 
 
 ?>
--- html/lib.php
+++ html/lib.php
@@ -1,5 +1,5 @@
 <?php
-function proc_call($procName, $procArg = NULL)
+function proc_call($queryText, $Student_ID = NULL, $Lecturer_ID = NULL, $Task_ID = NULL, $Class_Name = NULL)
 {
     $db_login='phpmyadmin';
     $db_host='localhost';
@@ -7,17 +7,61 @@ function proc_call($procName, $procArg = NULL)
     $db='TestDB';
     $conn=mysqli_connect($db_host,$db_login,$db_pass,$db) or die("Не могу подключиться к БД db: " . db_error());
     mysqli_query($conn, 'SET NAMES "utf8"');
-    
+    $where_clause = "";
+    $conditions = compact("Student_ID", "Lecturer_ID", "Task_ID", "Class_Name");
+    foreach ($conditions as $i => $value)
+    {
+        if ($value != NULL)
+        {
+            $where_clause .= $i."=".$value." ";
+            echo "\"".$where_clause."\"<br>";
+
+            
+        }
+    }
+    echo "<br><br><br><br><br>  ";
+
+    // echo "\"".$where_clause."\"<br>";
+    $where_clause = trim($where_clause);
+    echo "\"".$where_clause."\"<br>";
+    // $where_clause = str_replace("\n\n", " and ", $where_clause);
+    $where_clause = str_replace(" ", " and ", $where_clause);
+    $where_clause = ($where_clause != "") ? "where ".$where_clause : "";
+    echo "\"".$where_clause."\"<br>";
+    echo "\"".$queryText.$where_clause."\"<br>";
+
     $arg = ($procArg == NULL)? '' : $procArg;
-    $query = mysqli_query($conn, "CALL {$procName}({$arg})") or die(mysqli_error($conn));
+    $query = mysqli_query($conn, $queryText.$where_clause) or die(mysqli_error($conn));
     $answer=array(mysqli_fetch_assoc($query));
     
     while($ans=mysqli_fetch_assoc($query))
     {
         array_push($answer, $ans);
     }
+    print_r($answer);
     mysqli_close($conn);
     
     return $answer;
 }
+$personal_student_task = "select *
+from Student NATURAL JOIN Task_Student NATURAL JOIN Task";
+
+$group_student_task = "select *
+from Student NATURAL JOIN Student_Groups NATURAL JOIN `Groups` NATURAL JOIN Task_Group NATURAL JOIN Task";
+
+$class_student_task = "select *
+from Student NATURAL JOIN Student_Groups NATURAL JOIN `Groups` NATURAL JOIN Group_Class NATURAL JOIN Class NATURAL JOIN Task_Class NATURAL JOIN Task";
+
+$student_tasks = "select Student_ID,Task_ID,Name,Description from ({$personal_student_task}) as t 
+\nUNION\n
+select Student_ID,Task_ID,Name ,Description from ({$group_student_task}) as t1
+\nUNION\n
+select Student_ID,Task_ID,Name,Description from ({$class_student_task}) as t2 \n";
+
+$lec_grs = "select Task_ID,Description, Number,count(Student_ID) from ({$personal_student_task} NATURAL JOIN Task_Lecturer NATURAL JOIN Student_Groups NATURAL JOIN `Groups`) as t 
+\nUNION\n
+select Task_ID,Description, Number,count(Student_ID) from ({$group_student_task})  as t1
+\nUNION\n
+select Task_ID,Description, Number,count(Student_ID) from ({$class_student_task}) as t2 \n"
+
 ?>
