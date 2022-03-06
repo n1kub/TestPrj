a1e7e347c76e9754c896c513284b6b1245c72982
n1kub <n1kub@mail.ru>
2022-03-06 01:16:04 +0000

    Final version

--- html/get_new.php
+++ html/get_new.php
@@ -5,14 +5,12 @@ $lecturer_id = $_POST['lecturer'];
 $task_id = $_POST['task'];
 $class_id = $_POST['class'];
 
-$stud_tasks_answer = proc_call('stud_tasks', intval($stud_id));
-$amount_lecturer_answer = proc_call('amount_of_studs_for_task_of_lecturer', intval($lecturer_id));
-$task_sg_answer = proc_call('task_studs_groups', intval($task_id));
-$class_studs_answer = proc_call('class_studs_list', '"'.$class_id.'"');
+list($stud_tasks_answer, $amount_lecturer_answer, 
+$task_sg_answer, $class_studs_answer) = get_data($stud_id, $lecturer_id, $task_id, $class_id);
 
 echo json_encode(array(
     array('stud' => $stud_tasks_answer, 'lecturer' => $amount_lecturer_answer, 
         'task' => $task_sg_answer, 'class' => $class_studs_answer), 
     array('stud' => array_keys($stud_tasks_answer[0]), 'lecturer' => array_keys($amount_lecturer_answer[0]),
-        'task' => array_keys($task_sg_answer[0]),'class' => array_keys($class_studs_answer[0]))));
+        'task' => array_keys($task_sg_answer[0]),'class' => array_keys($class_studs_answer[0])), count($amount_lecturer_answer)));
 ?>
--- html/lib.php
+++ html/lib.php
@@ -1,5 +1,5 @@
 <?php
-function proc_call($queryText, $Student_ID = NULL, $Lecturer_ID = NULL, $Task_ID = NULL, $Class_Name = NULL)
+function execute_query($queryText)
 {
     $db_login='phpmyadmin';
     $db_host='localhost';
@@ -7,28 +7,6 @@ function proc_call($queryText, $Student_ID = NULL, $Lecturer_ID = NULL, $Task_ID
     $db='TestDB';
     $conn=mysqli_connect($db_host,$db_login,$db_pass,$db) or die("Не могу подключиться к БД db: " . db_error());
     mysqli_query($conn, 'SET NAMES "utf8"');
-    $where_clause = "";
-    $conditions = compact("Student_ID", "Lecturer_ID", "Task_ID", "Class_Name");
-    foreach ($conditions as $i => $value)
-    {
-        if ($value != NULL)
-        {
-            $where_clause .= $i."=".$value." ";
-            echo "\"".$where_clause."\"<br>";
-
-            
-        }
-    }
-    echo "<br><br><br><br><br>  ";
-
-    // echo "\"".$where_clause."\"<br>";
-    $where_clause = trim($where_clause);
-    echo "\"".$where_clause."\"<br>";
-    // $where_clause = str_replace("\n\n", " and ", $where_clause);
-    $where_clause = str_replace(" ", " and ", $where_clause);
-    $where_clause = ($where_clause != "") ? "where ".$where_clause : "";
-    echo "\"".$where_clause."\"<br>";
-    echo "\"".$queryText.$where_clause."\"<br>";
 
     $arg = ($procArg == NULL)? '' : $procArg;
     $query = mysqli_query($conn, $queryText.$where_clause) or die(mysqli_error($conn));
@@ -38,30 +16,94 @@ function proc_call($queryText, $Student_ID = NULL, $Lecturer_ID = NULL, $Task_ID
     {
         array_push($answer, $ans);
     }
-    print_r($answer);
     mysqli_close($conn);
     
     return $answer;
 }
-$personal_student_task = "select *
-from Student NATURAL JOIN Task_Student NATURAL JOIN Task";
 
-$group_student_task = "select *
-from Student NATURAL JOIN Student_Groups NATURAL JOIN `Groups` NATURAL JOIN Task_Group NATURAL JOIN Task";
+function get_data($s_id = 1, $l_id = 1, $t_id = 3, $c_name = 'Экономика в информационных технологиях')
+{
+    $q1 = "select *
+    from Student NATURAL JOIN Task_Student NATURAL JOIN Task  NATURAL JOIN 
+    Task_Lecturer NATURAL JOIN Student_Groups NATURAL JOIN `Groups` ";
 
-$class_student_task = "select *
-from Student NATURAL JOIN Student_Groups NATURAL JOIN `Groups` NATURAL JOIN Group_Class NATURAL JOIN Class NATURAL JOIN Task_Class NATURAL JOIN Task";
+    $q2 = "select *
+    from Student NATURAL JOIN Student_Groups NATURAL JOIN `Groups` NATURAL JOIN Task_Group NATURAL JOIN Task ";
 
-$student_tasks = "select Student_ID,Task_ID,Name,Description from ({$personal_student_task}) as t 
-\nUNION\n
-select Student_ID,Task_ID,Name ,Description from ({$group_student_task}) as t1
-\nUNION\n
-select Student_ID,Task_ID,Name,Description from ({$class_student_task}) as t2 \n";
+    $q3 = "select *
+    from Student NATURAL JOIN Student_Groups NATURAL JOIN `Groups` NATURAL JOIN 
+    Group_Class NATURAL JOIN Task_Class NATURAL JOIN Task ";
 
-$lec_grs = "select Task_ID,Description, Number,count(Student_ID) from ({$personal_student_task} NATURAL JOIN Task_Lecturer NATURAL JOIN Student_Groups NATURAL JOIN `Groups`) as t 
-\nUNION\n
-select Task_ID,Description, Number,count(Student_ID) from ({$group_student_task})  as t1
-\nUNION\n
-select Task_ID,Description, Number,count(Student_ID) from ({$class_student_task}) as t2 \n"
+    $q4 = "select *
+    from Lecturer NATURAL JOIN Task_Lecturer ";
+
+    $q5 = "select * 
+    from Class NATURAL JOIN Group_Class NATURAL JOIN `Groups` NATURAL JOIN Student_Groups NATURAL JOIN Student ";
+
+    $stud_tasks_data = execute_query($q1);
+    foreach (execute_query($q2) as $i => $value)
+    {
+        array_push($stud_tasks_data, $value);
+    }
+    foreach (execute_query($q3) as $i => $value)
+    {
+        array_push($stud_tasks_data, $value);
+    }
+    $lecturer_data = execute_query($q4);
+    $class_data = execute_query($q5);
+
+    $stud_tasks_answer = array();
+    foreach ($stud_tasks_data as $i => $value)
+    {
+        if ($value['Student_ID'] == $s_id)
+        {
+            array_push($stud_tasks_answer, array('Task_ID' => $value['Task_ID'], 
+            'Name' => $value['Name'], 'Description' => $value['Description']));
+        }
+    }
+    $stud_tasks_answer = array_unique($stud_tasks_answer, SORT_REGULAR);
+
+    
+    $amount_lecturer_answer = array();
+    foreach ($lecturer_data as $i => $value)
+    {
+        if ($value['Lecturer_ID'] == $l_id)
+        {
+            foreach ($stud_tasks_data as $num => $data)
+            {
+                if ($data['Task_ID']==$value['Task_ID'])
+                {
+                    array_push($amount_lecturer_answer, array('Full_name' => $data['Full_name'], 'Number' => $data['Number'], 'Task_ID' => $data['Task_ID'], 
+                    'Name' => $data['Name'], 'Description' => $data['Description']));
+                }
+            }
+        }
+    }
+    $amount_lecturer_answer = array_unique($amount_lecturer_answer, SORT_REGULAR);
+    $task_sg_answer = array();
+    foreach ($stud_tasks_data as $i => $value)
+    {
+        
+        if ($value['Task_ID'] == $t_id)
+        {
+            
+            array_push($task_sg_answer, array('Full_name' => $value['Full_name'], 'Number' => $value['Number']));
+        }
+    }
+    $task_sg_answer = array_unique($task_sg_answer, SORT_REGULAR);
+
+    $class_studs_answer = array();
+    foreach ($class_data as $i => $value)
+    {
+        if ($value['Class_Name'] == $c_name)
+        {
+            array_push($class_studs_answer, array('Full_name' => $value['Full_name'], 'Number' => $value['Number']));
+        }
+    }
+    $class_studs_answer = array_unique($class_studs_answer, SORT_REGULAR);
+
+    return array($stud_tasks_answer, $amount_lecturer_answer, $task_sg_answer,
+     $class_studs_answer, $lecturer_data, $stud_tasks_data);
+}
 
 ?>
